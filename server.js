e167f57145f539df67ce6803c1a8280f7bd76254
emalv045 <43219795+emalv045@users.noreply.github.com>
2019-04-19 17:55:25 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+require('./models/db');
+
+const express=require('express');
+const path=require('path');
+const exphbs=require('express-handlebars');
+const bodyparser=require('body-parser');
+
+const indexController=require('./controllers/indexController');
+const vehiculoController=require('./controllers/vehiculoController');
+const clienteController=require('./controllers/clienteController');
+
+
+var app=express();
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+
+app.set("views",path.join(__dirname,'/views/'));
+app.engine('hbs',exphbs({extname:'hbs',defaultLayout:'mainLayout',layoutsDir: __dirname+'/views/layouts/'}));
+app.set('view engine','hbs');
+
+
+
+app.listen(3000,()=>{
+    console.log('Express server started at port: 3000');
+});
+
+
+app.use('/',indexController);
+app.use('/vehiculo',vehiculoController);
+app.use('/cliente',clienteController);
+
+
+
